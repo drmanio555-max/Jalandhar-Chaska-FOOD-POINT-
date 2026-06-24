1a4d76ebc03967911da92b81248cf73539c19c49
drmanio555-max <drmanio555@gmail.com>
2026-06-25 00:45:40 +0500

    Add new dish entry in Script.java

---
+++ Script.java
@@ -0,0 +1,13 @@
+// نئی ڈش شامل کرنا:
+{
+    id: 13,
+    name: "اپنی ڈش کا نام (اردو/انگلش)",
+    price: قیمت,
+    description: "مختصر تفصیل",
+    image: "تصویر کا لنک",
+    category: "dahi", // یا fruit, burger, zinger, shawarma, chips
+    rating: 4.5,
+    deliveryTime: "10-15 min",
+    badge: "Popular 🔥", // یا "New 🎉" یا "Offer 🎊" یا ""
+    inStock: true
+}
